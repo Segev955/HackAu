8b461e9a7e3c5ac96232944b5337d649f5a5f23d
SaliSharfman <86108478+SaliSharfman@users.noreply.github.com>
2022-04-25 19:36:48 +0300

    Delete HostGui.py

--- HostGui.py
+++
@@ -1,8 +0,0 @@
-from tkinter import Tk
-from users import*
-from main import *
-def host_screen(user:User):
-    home_screen = Toplevel(screen)
-    home_screen.title("Home Page")
-    home_screen.geometry("300x250")
-    create_menu_bar(home_screen)
