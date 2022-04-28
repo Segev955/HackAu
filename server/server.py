cb5eb554598cdc9c490cc4bd60e70c7487de7d0f
SaliSharfman <86108478+SaliSharfman@users.noreply.github.com>
2022-04-28 03:15:42 +0300

    create server

---
+++ server/server.py
@@ -0,0 +1,178 @@
+from datetime import time
+from socket import *
+from threading import Thread
+import time
+from users import *
+
+# GLOBAL CONSTANTS
+HOST = ''
+PORT = 55000
+ADDR = (HOST, PORT)
+MAX_CONNETIONS = 10
+BUFSIZ = 1024
+
+# GLOBAL VARIABLES
+u = Users()
+onlines = []
+
+# set up server
+SERVER = socket(AF_INET, SOCK_STREAM)
+SERVER.bind(ADDR)
+
+
+class Online:
+    global counter
+    counter=0
+    def __init__(self, addr, sock, name=f"online{counter}"):
+        self.addr = addr
+        self.sock = sock
+        self.name = name
+
+    def set_name(self, name:str):
+        self.name = name
+    def __repr__(self) -> str:
+        return self.name
+
+
+def date_from_str(date: str, spliter='/') -> tuple:
+    lst=[]
+    lst.append(int(date.split(spliter)[1]))
+    lst.append(int(date.split(spliter)[0]))
+    lst.append(int(date.split(spliter)[2]))
+    tup = [int(date.split(spliter)[1]), int(date.split(spliter)[0]), int(date.split(spliter)[2])]
+    return lst
+
+
+def broadcast(msg):
+    """
+    send new messages to all clients
+    :param msg: bytes["utf8"]
+    :param name: str
+    :return: None
+    """
+    for online in onlines:
+        sock = online.sock
+        try:
+            sock.send(bytes(msg, "utf8"))
+        except Exception as e:
+            print("[EXCEPTION]", e)
+
+def sendto(msg, name):
+    """
+    send new messages to all clients
+    :param msg: bytes["utf8"]
+    :param name: str
+    :return: None
+    """
+    for online in onlines:
+        if online.name == name:
+            sock = online.sock
+            try:
+                sock.send(bytes(f"{msg}", "utf8"))
+            except Exception as e:
+                print("[EXCEPTION]", e)
+
+
+def get_online_index(name):
+    for i in range(len(onlines)):
+        if onlines[i].name==name:
+            return i
+    return None
+def rename_client(online:Online,newname:str):
+    sendto(f"RENAME,{newname}", online.name)
+    time.sleep(0.1)
+    onlines.pop(get_online_index(online.name))
+    online.name=newname
+    onlines.append(online)
+    # oldonline=get_online(oldname)
+    # newonline=Online(oldonline.addr,oldonline.sock,newname)
+    # onlines.remove(oldonline)
+    # onlines.append(newonline)
+def client_communication(online: Online):
+    """
+    Thread to handle all messages from client
+    :param person: Person
+    :return: None
+    """
+
+
+    # first message received is always the persons name
+    onlines.append(online)
+    name = f"CLIENT~{len(onlines)}"
+    rename_client(online,name)
+    msg = f"{onlines[get_online_index(name)].name} has connected to the Server!"
+    broadcast(msg)  # broadcast welcome message
+    print(f"Online users: {onlines}")
+    client = onlines[get_online_index(name)].sock
+    while True:  # wait for any messages from person
+        msg = client.recv(BUFSIZ).decode("utf8")
+        # time.sleep(1)
+        sp=[]
+        try:
+            sp = msg.split(',')
+        except:
+            break
+
+        if msg == "{quit}":  # if message is qut - so disconnect the client
+            onlines.remove(online)
+            print(f"Online users: {onlines}")
+
+        elif sp[0] == "NEWUSER":
+            user = User(username=sp[1], password=sp[2], gender=sp[3], bdate=date_from_str(sp[4], '.'), type=sp[5])
+            f, msg = u.newuser(user)
+            sendto(f"NEWUSER,{f},{msg}", online.name)
+        elif sp[0] == "CHECKTYPE":
+            for i in u.p.values():
+                if sp[1] == i.username:
+                    if i.type == 'Host':
+                        sendto(f"TYPE,{i.username},HOST", online.name)
+                    else:
+                        sendto(f"TYPE,{i.username},GUEST", online.name)
+                    break
+        elif sp[0] == "CHECKPASS":
+            f, msg = u.chekcpass(sp[1], sp[2])
+            sendto(f"CHECKPASS,{f},{msg}", online.name)
+        elif sp[0] == "RENAME":
+            rename_client(onlines[get_online_index(sp[1])],sp[2])
+
+        elif msg == "USERTOSTRING":
+            if online.name in u.p.keys():
+                sendto(f"USERTOSTRING,{u.p[online]}",online.name)
+
+
+        else:
+            print(f"Unknown Message: {msg}")
+
+
+def wait_for_connection():
+    """
+    Wait for connecton from new clients, start new thread once connected
+    :return: None
+    """
+    while True:
+        try:
+            sock, addr = SERVER.accept()  # wait for any new connections
+            online = Online(addr, sock)  # create new person for connection
+
+            now= datetime.datetime.now().strftime("%H:%M:%S")
+            print(f"[CONNECTION] {addr} connected to the server at {now}")
+            Thread(target=client_communication, args=(online,)).start()
+        except Exception as e:
+            print("[EXCEPTION]", e)
+            break
+
+    print("SERVER CRASHED")
+
+
+def start_server():
+    SERVER.listen(MAX_CONNETIONS)  # open server to listen for connections
+    print("[STARTED] Waiting for connections...")
+    ACCEPT_THREAD = Thread(target=wait_for_connection)
+    ACCEPT_THREAD.start()
+    ACCEPT_THREAD.join()
+    SERVER.close()
+
+
+def stop_server():
+    SERVER.close()
+start_server()
---
+++ server/users.py
@@ -0,0 +1,92 @@
+import datetime
+import json
+
+
+class User:
+    def __init__(self, username: str, password: str, gender: str, bdate: list, type: str):
+        self.username = username
+        self.password = password
+        self.gender = gender
+        self.bdate = (bdate[2],bdate[1],bdate[0])
+        self.type = type
+
+
+    def getPass(self):
+        return self.password
+
+    def __repr__(self):
+        return f"password: {self.password}, Birth Date: {self.bdate[0]}/{self.bdate[1]}/{self.bdate[2]}, User Type: {self.type}."
+    def __str__(self):
+        return f"{self.type} User {self.username}:\nBirth Date: {self.bdate[0]}/{self.bdate[1]}/{self.bdate[2]}\ngender: {self.gender}\n"
+
+
+class Users:
+    def __init__(self):
+        self.p = {}
+
+    def newuser(self, user: User):
+        self.load_from_json()
+        if user.username=="":
+            return False, "Please input Username."
+        if user.password=="":
+            return False, "Please input Password."
+        if user.gender is None:
+            return False, "Please choose Gender."
+        if user.bdate is None:
+            return False, "Please choose Birth Date."
+        if user.bdate[0]==-1:
+                return False, "Please choose Year."
+        if user.bdate[1]==-1:
+                return False, "Please choose Month."
+        if user.bdate[2]==-1:
+                return False, "Please choose Day."
+        if user.type is None:
+            return False, "Please choose Your Type."
+        if user.username in self.p.keys():
+            return False, "Username Exists."
+        if len(user.password) < 5:
+            return False, "Password Too Short."
+        if len(user.username) < 5:
+            return False, "Username Too Short."
+        for c in user.username:
+            if not c.isalpha() and not c.isnumeric() and not c in "_-.":
+                return False, "Illegal Username."
+        for c in user.password:
+            if not c.isalpha() and not c.isnumeric() and not c in "_-.":
+                return False, "Illegal Password."
+
+        self.p[user.username] = user
+        self.save_to_json(user)
+        self.load_from_json()
+        return True, f'Welcome {user.username}!'
+
+    def chekcpass(self, username: str, password: str):
+        self.load_from_json()
+        if username=="":
+            return False, "Please Input Username."
+        if password=="":
+            return False, "Please Input Password."
+        if username not in self.p.keys():
+            return False, "Username not exist!"
+        if self.p[username].getPass() != password:
+            return False, "Wrong password!"
+        return True, f'Welcome {username}!'
+
+    def deluser(self, username: str, password: str):
+        if self.chekcpass(username, password):
+            self.p.pop(username)
+            return True
+        return False
+
+    def save_to_json(self, user:User, file_name="users.json"):
+        with open(file_name) as f:
+            j = json.load(f)
+        j[user.username] = (user.username,user.password,user.gender, user.bdate, user.type)
+        with open(file_name, 'w') as jf:
+            json.dump(j, jf, indent=4, separators=(',', ': '))
+
+    def load_from_json(self, file_name="users.json"):
+        with open(file_name) as f:
+            dict = json.load(f)
+            for name, user in dict.items():
+                self.p[name]=User(user[0],user[1],user[2],user[3],user[4])
